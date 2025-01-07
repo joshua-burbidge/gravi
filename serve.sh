998b51b86882aeb9b74cc9f4c4cd980917fb0dfa
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-01-07 06:05:11 -0600

    update name

--- serve.sh
+++ serve.sh
@@ -1,6 +1,6 @@
 rustup target add wasm32-unknown-unknown
 cargo build --target=wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
-wasm-bindgen ./target/wasm32-unknown-unknown/debug/femtovg-wgpu.wasm --out-dir wasm/generated --target web
+wasm-bindgen ./target/wasm32-unknown-unknown/debug/grav.wasm --out-dir wasm/generated --target web
 cd wasm
 python3 -m http.server
