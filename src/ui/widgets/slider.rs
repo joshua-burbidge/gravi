8573c5611ff8681ca73c117092148dd93a70a84e
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-02-18 07:08:19 -0600

    custom formatter for slider

--- src/ui/widgets/slider.rs
+++ src/ui/widgets/slider.rs
@@ -1,6 +1,6 @@
 use std::ops::RangeInclusive;
 
-use egui::emath;
+use egui::emath::{self, round_to_decimals};
 
 pub struct CustomSlider<'a, Num: emath::Numeric> {
     value: &'a mut Num,
@@ -30,13 +30,23 @@ impl<'a, Num: emath::Numeric> CustomSlider<'a, Num> {
     }
 }
 
-// fn custom_formatter(ui: egui::Ui, num: f64, range: RangeInclusive<usize>) -> String {
-//     if num < 1000. {
-//         ui.style().number_formatter.format(num, range)
-//     } else {
+fn custom_formatter(
+    default_formatter: &egui::style::NumberFormatter,
+    num: f64,
+    range: RangeInclusive<usize>,
+) -> String {
+    if num <= 100000. {
+        default_formatter.format(num, range)
+    } else {
+        let exponent = num.log10().floor() as i32;
 
-//     }
-// }
+        let decimal = num / (10_f64.powi(exponent));
+
+        let rounded = round_to_decimals(decimal, 3);
+
+        String::from(format!("{}e{}", rounded, exponent))
+    }
+}
 
 impl<'a, Num: emath::Numeric> egui::Widget for CustomSlider<'a, Num> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
@@ -52,17 +62,40 @@ impl<'a, Num: emath::Numeric> egui::Widget for CustomSlider<'a, Num> {
 
             // scope to change width of this slider only
             ui.scope(|ui| {
+                let available_width = ui.available_width();
+                let style = ui.style_mut();
+
                 if self.full_width {
-                    ui.style_mut().spacing.slider_width = ui.available_width() - 70.0;
+                    style.spacing.slider_width = available_width - 70.0;
                 }
 
-                let slider =
-                    egui::Slider::new(self.value, self.range).clamping(egui::SliderClamping::Never);
+                let def_formatter = &style.number_formatter.clone();
+
+                let formatter =
+                    |num, range| -> String { custom_formatter(&def_formatter, num, range) };
+
+                let slider = egui::Slider::new(self.value, self.range)
+                    .clamping(egui::SliderClamping::Never)
+                    .custom_formatter(formatter);
 
                 response = Some(ui.add(slider));
             })
         });
 
         response.unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn formatter() {
+        let num = 0_f64;
+
+        let exponent = num.log10().floor() as i32;
+
+        assert_eq!(exponent, 1);
     }
 }
