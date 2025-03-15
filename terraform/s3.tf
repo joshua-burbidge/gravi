0d1df6b9e2d0139819fbb3901ce7c21dff2bf00f
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-03-15 15:59:00 -0500

    fix arn input

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -59,7 +59,7 @@ data "aws_iam_policy_document" "allow_public_read_access" {
     ]
 
     resources = [
-      "${aws_s3_bucket.example.arn}/*",
+      "${aws_s3_bucket.website_bucket.arn}/*",
     ]
   }
 }
