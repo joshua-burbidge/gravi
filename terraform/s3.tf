25e813d1d4059ce50e9ef7676039d5a64d500393
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-03-15 16:00:03 -0500

    disable all public access blocks

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 
   block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
