0d30451707b667850cbacaa03a469add33fc9d1b
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-03-15 15:42:48 -0500

    make bucket public read only

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "gravi_bucket" {
+resource "aws_s3_bucket" "website_bucket" {
   bucket = "gravi-bucket"
 }
 
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.gravi_bucket.id
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.id
 
   index_document {
     suffix = "index.html"
@@ -11,4 +11,30 @@ resource "aws_s3_bucket_website_configuration" "example" {
   error_document {
     key = "error.html"
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "controls" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.controls,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+
+  bucket = aws_s3_bucket.website_bucket
+  acl    = "public-read"
 }
