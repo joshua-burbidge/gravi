a3f2e912924917005e7fb37f7014f702d28612c5
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-03-15 20:53:27 -0500

    try debug build

--- build-release.sh
+++ build-release.sh
@@ -1,4 +1,4 @@
 rustup target add wasm32-unknown-unknown
-cargo build --target=wasm32-unknown-unknown --release
+cargo build --target=wasm32-unknown-unknown
 cargo install wasm-bindgen-cli --version 0.2.100
-wasm-bindgen ./target/wasm32-unknown-unknown/release/grav.wasm --out-dir web/generated --target web
+wasm-bindgen ./target/wasm32-unknown-unknown/debug/grav.wasm --out-dir web/generated --target web
