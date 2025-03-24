96b77b9026fc91d35bebf6fcbc9a28dca9b4b28b
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-03-24 09:16:49 -0500

    add another bench for run function

--- benches/real-bench.rs
+++ benches/real-bench.rs
@@ -1,18 +1,38 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use femtovg::Color;
 use grav::{
-    app::App,
     helpers::{init_canvas, wgpu::create_canvas},
-    Orbital,
+    App, Orbital,
 };
 
-fn bench_batched(c: &mut Criterion) {
-    c.bench_function("run_bench", |b| {
+// bench the first call of the "run" function
+fn run_bench(c: &mut Criterion) {
+    c.bench_function("first_run_bench", |b| {
         b.iter_batched_ref(
             || {
                 let mut app = Orbital::new();
                 app.load_preset(1);
                 app.start();
+                app
+            },
+            |app| app.run(),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+// bench a call of the "run" function with many ticks already calculated
+fn long_run_bench(c: &mut Criterion) {
+    c.bench_function("long_run_bench", |b| {
+        b.iter_batched_ref(
+            || {
+                let mut app = Orbital::new();
+                app.load_preset(1);
+                app.num_ticks = 1e6 as i32;
+                app.start();
+                app.run();
+                app.run();
+                app.run();
                 app
             },
             |app| app.run(),
@@ -45,5 +65,5 @@ fn draw_bench(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_batched, draw_bench);
+criterion_group!(benches, run_bench, long_run_bench, draw_bench);
 criterion_main!(benches);
