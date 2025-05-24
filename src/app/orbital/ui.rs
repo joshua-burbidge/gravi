5ef6acedea0a1372244715953f91954dac3a3072
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2025-05-24 10:36:38 -0500

    add text wrapping to collapsing sections

--- src/app/orbital/ui.rs
+++ src/app/orbital/ui.rs
@@ -51,62 +51,92 @@ pub fn ui(app: &mut Orbital, ctx: &egui::Context) {
             let x_range = -10000.0..=10000.;
             let y_range = -10000.0..=10000.;
 
-            egui::CollapsingHeader::new(RichText::new(bodies_list[i].clone()).heading().color(
-                egui::Color32::from_rgb(body.color.0, body.color.1, body.color.2),
-            ))
-            .default_open(body.default_expanded)
-            .show(ui, |ui| {
-                ui.add_enabled_ui(!started, |ui| {
-                    text_sized(ui, "Position (km)", 14.);
-                    ui.add(XYInput::new(
-                        &mut body.absolute_pos.x,
-                        &mut body.absolute_pos.y,
-                        x_range,
-                        y_range,
-                    ));
-                    ui.label(format!("|r|: {} km", body.pos.mag()));
-                    ui.add_space(6.);
+            let id = ui.make_persistent_id(format!("collapsing_{}", i));
+            let collapsing = egui::collapsing_header::CollapsingState::load_with_default_open(
+                ui.ctx(),
+                id,
+                body.default_expanded,
+            );
 
-                    if !body.is_fixed {
-                        text_sized(ui, "Velocity (km/s)", 14.);
-                        ui.checkbox(
-                            &mut body.lock_to_circular_velocity,
-                            "lock to circular velocity",
-                        );
-                        ui.checkbox(&mut body.lock_to_escape_velocity, "lock to escape velocity");
+            let response = collapsing
+                .show_header(ui, |ui| {
+                    ui.add(
+                        egui::Label::new(RichText::new(bodies_list[i].clone()).heading().color(
+                            egui::Color32::from_rgb(body.color.0, body.color.1, body.color.2),
+                        ))
+                        .wrap(),
+                    )
+                })
+                .body(|ui| {
+                    ui.add_enabled_ui(!started, |ui| {
+                        text_sized(ui, "Position (km)", 14.);
+                        ui.add(XYInput::new(
+                            &mut body.absolute_pos.x,
+                            &mut body.absolute_pos.y,
+                            x_range,
+                            y_range,
+                        ));
+                        ui.label(format!("|r|: {} km", body.pos.mag()));
+                        ui.add_space(6.);
 
-                        let vel_lock_enabled =
-                            body.lock_to_circular_velocity || body.lock_to_escape_velocity;
-                        ui.add_enabled_ui(vel_lock_enabled, |ui| {
-                            egui::ComboBox::from_label("Around body").show_index(
-                                ui,
-                                &mut body.selected_vel_lock,
-                                bodies_list.len(),
-                                |i| bodies_list[i].clone(),
+                        if !body.is_fixed {
+                            text_sized(ui, "Velocity (km/s)", 14.);
+                            ui.checkbox(
+                                &mut body.lock_to_circular_velocity,
+                                "lock to circular velocity",
                             );
-                        });
-                        ui.add_enabled_ui(!vel_lock_enabled, |ui| {
-                            ui.add(XYInput::new(
-                                &mut body.v.x,
-                                &mut body.v.y,
-                                -50.0..=50.0,
-                                -50.0..=50.0,
-                            ));
-                        });
-                        ui.label(format!("|v|: {} km/s", body.v.mag()));
+                            ui.checkbox(
+                                &mut body.lock_to_escape_velocity,
+                                "lock to escape velocity",
+                            );
 
-                        ui.add_space(4.);
-                    }
+                            let vel_lock_enabled =
+                                body.lock_to_circular_velocity || body.lock_to_escape_velocity;
+                            ui.add_enabled_ui(vel_lock_enabled, |ui| {
+                                egui::ComboBox::from_label("Around body").show_index(
+                                    ui,
+                                    &mut body.selected_vel_lock,
+                                    bodies_list.len(),
+                                    |i| bodies_list[i].clone(),
+                                );
+                            });
+                            ui.add_enabled_ui(!vel_lock_enabled, |ui| {
+                                ui.add(XYInput::new(
+                                    &mut body.v.x,
+                                    &mut body.v.y,
+                                    -50.0..=50.0,
+                                    -50.0..=50.0,
+                                ));
+                            });
+                            ui.label(format!("|v|: {} km/s", body.v.mag()));
 
-                    text_sized(ui, "Mass (kg)", 14.);
-                    ui.add(CustomSlider::new(&mut body.mass, 1.0..=5e10).label("M:"));
-                    ui.add_space(6.);
+                            ui.add_space(4.);
+                        }
 
-                    text_sized(ui, "Acceleration (km/s^2)", 14.);
-                    ui.monospace(format!("Ax:    {:+.4e}", body.computed_a.x));
-                    ui.monospace(format!("Ay:    {:+.4e}", body.computed_a.y));
+                        text_sized(ui, "Mass (kg)", 14.);
+                        ui.add(CustomSlider::new(&mut body.mass, 1.0..=5e10).label("M:"));
+                        ui.add_space(6.);
+
+                        text_sized(ui, "Acceleration (km/s^2)", 14.);
+                        ui.monospace(format!("Ax:    {:+.4e}", body.computed_a.x));
+                        ui.monospace(format!("Ay:    {:+.4e}", body.computed_a.y));
+                    });
                 });
-            });
+
+            // TODO cursor icon when clicking
+            if response.1.inner.hovered() {
+                ui.ctx().set_cursor_icon(egui::CursorIcon::Default);
+            }
+
+            if response.1.inner.clicked() {
+                if let Some(mut col_state) =
+                    egui::collapsing_header::CollapsingState::load(ui.ctx(), id)
+                {
+                    col_state.toggle(ui);
+                    col_state.store(ui.ctx());
+                }
+            }
+
             ui.add_space(20.);
         }
         app.set_velocities();
